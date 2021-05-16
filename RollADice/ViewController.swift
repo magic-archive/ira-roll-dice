2bcc0a7185f7379e105805d8216ee8e57ef230d3
Ira Pramanick <irapramanick@gmail.com>
2021-05-15 17:12:27 -0700

    Commiting.
    Signed-off-by: Ira Pramanick <irapramanick@gmail.com>

--- RollADice/ViewController.swift
+++ RollADice/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollDice(_ sender: Any) {
+        let number = (1...6).randomElement()!
+        self.diceImageView.image = UIImage(named: "dice-\(number)")
+    }
+    
 }
 
